ff27c36c3ad3c468ecd41e200a518547654def8d
Evgeniya Kotova <zheniyasl@gmail.com>
2021-08-05 17:20:19 +0200

    get initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import React, { Component } from "react";
+import "../App.css";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
-function App() {
-  return <div className="App">App</div>;
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return <div>App</div>;
+  }
 }
 
-export default App;
+export default connect()(App);
--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,7 +2,7 @@ const logger = (store) => (next) => (action) => {
   console.group(action.type);
   console.log("The action: ", action);
   const result = next(action);
-  console.log("The new state: ", store.getState);
+  console.log("The new state: ", store.getState());
   console.groupEnd();
   return result;
 };
