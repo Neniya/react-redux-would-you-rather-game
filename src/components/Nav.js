9abcacad4d83e9ae4f6f4bd4c63b42c0485508ff
Evgeniya Kotova <zheniyasl@gmail.com>
2021-08-12 17:12:59 +0200

    impove and style Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default function Nav() {
-  return (
-    <nav className="nav">
-      <ul>
-        <li>
-          <NavLink to="/" exact activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/new" activeClassName="active">
-            New question
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+class Nav extends Component {
+  render() {
+    console.log("nav", this.props);
+    const user = this.props.user;
+    return (
+      <nav className="nav">
+        <ul>
+          <li>
+            <NavLink
+              to="/dashboard"
+              exact
+              activeClassName="active"
+              className="nav-link"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new" activeClassName="active" className="nav-link">
+              New question
+            </NavLink>
+          </li>
+        </ul>
+        <div>
+          <p className="user-name">
+            <b>Hello</b>, {user ? user.name : ""}
+          </p>
+        </div>
+      </nav>
+    );
+  }
 }
+
+function mapStateToProps({ users, authedUser }) {
+  return {
+    user: users[authedUser],
+  };
+}
+
+export default connect(mapStateToProps)(Nav);
